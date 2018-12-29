efb0b78448e86ee3d9e33ed125d5e0799b9982a9
FransRiz <Fransiskus.0084@students.amikom.ac.id>
2018-12-29 09:16:36 +0700

    responsi uas

---
+++ application/views/home/index.php
@@ -0,0 +1,10 @@
+<!--<div class="container">
+	<div class="jumbotron mt-4">
+	  <h1 class="display-4">Selamat Datang.....</h1>
+	  <p class="lead">Di Tugas "CRUD" Responsi Pemrograman Web Lanjut  </p>
+	  <hr class="my-4">
+	  <p class="lead">
+	    <a class="btn btn-primary btn-lg" href="members" role="button">Mulai</a>
+	  </p>
+	</div>
+</div>->
---
+++ application/views/templates/footer.php
@@ -0,0 +1,20 @@
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <!--  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="<?= base_url(); ?>assets/js/sweetalert2.all.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/script.js"></script>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+<div id="footer_bottom" class="footer-bottom">
+<div class="container">
+<div class="row">
+<div class="col-md-20">
+<div class="copyright">2018 © </a> All rights reserved.</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html> -->
